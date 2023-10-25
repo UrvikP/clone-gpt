184506dabf5dfa7ecd31337243b8d45d1ae21f48
UrvikP <urvik95@gmail.com>
2023-10-25 15:56:50 -0400

    updated main page, added icon/images

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ function App() {
   return (
     <div className="App">
       <div className='sidebar'>
-        Hello
+        cloneGPT
         <div className='upperSide'>
-          <div className='upperSideTop'> <img src='' alt='' className='logo'/> <span>cloneGPT</span></div>
+          <div className='upperSideTop'> <img src='' alt='' className='logo'/> <span className='brand'>cloneGPT</span></div>
+          <button className='midBtn'> <img src='' alt='' className='addBtn' />New Chat</button>
+          <div className='upperSideBottom'>
+            <button className='query'> <img src='' alt='' />What is Programming?</button>
+            <button className='query'> <img src='' alt='' />What is Programming?</button>
+          </div>
         </div>
         <div className='lowerSide'>
           lower
